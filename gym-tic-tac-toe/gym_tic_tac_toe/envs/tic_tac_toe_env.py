7419eb7e84ea9bd7bc26513ea89b0a3da5b005be
Akhilez <lightrescuer@gmail.com>
2020-12-23 23:39:59 -0600

    Added more env stuff

--- gym-tic-tac-toe/gym_tic_tac_toe/envs/tic_tac_toe_env.py
+++ gym-tic-tac-toe/gym_tic_tac_toe/envs/tic_tac_toe_env.py
@@ -1,7 +1,43 @@
 import gym
-from gym import error, spaces, utils
-from gym.utils import seeding
+from gym import spaces
 import numpy as np
+
+
+class Pix:
+    class X:
+        string = 'X'
+        tup = (0, 1, 0)
+        arr = np.array(tup)
+
+    class O:
+        string = 'O'
+        tup = (0, 0, 1)
+        arr = np.array(tup)
+
+    class S:
+        string = '_'
+        tup = (1, 0, 0)
+        arr = np.array(tup)
+
+    pix_to_str = {
+        X.tup: X.string,
+        O.tup: O.string,
+        S.tup: S.string
+    }
+    str_to_tup = {
+        X.string: X.tup,
+        O.string: O.tup,
+        S.string: S.tup
+    }
+
+
+Xs = Pix.X.string
+Os = Pix.O.string
+Ss = Pix.S.string
+
+Xt = Pix.X.tup
+Ot = Pix.O.tup
+St = Pix.S.tup
 
 
 class TicTacToeEnv(gym.Env):
@@ -14,12 +50,42 @@ class TicTacToeEnv(gym.Env):
         # Example for using image as input:
         self.observation_space = spaces.Box(low=0, high=1, shape=(3, 3), dtype=np.uint8)
 
-    def step(self, action):
+        self.state = None
+        self.is_done = False
+
+    def step(self, action: int):
         # return next_state, reward, is_done, info
+        i = 9 // action
+        j = 9 % action
+
+        # Check if the action is LEGAL or not
+        if self.state[i][j] == Pix.S.arr:
+            prev_state = np.copy(self.state)
+            self.state[i][j] = Pix.X.arr
+
         return 1
 
     def reset(self):
-        return 0
+        self.state = self._get_empty_state()
+        self.is_done = False
+        return self.state
 
     def render(self, mode='human', close=False):
-        print("hello")
+        string = []
+        for r in range(3):
+            for c in range(3):
+                pixel = self.state[r, c, :]
+                string.append(Pix.pix_to_str[tuple(pixel)])
+            string.append('\n')
+        state_str = ''.join(string)
+        print(state_str)
+
+    def set_state(self, state_str):
+        state = np.array([[Pix.str_to_tup[pix] for pix in row] for row in state_str])
+        self.state = state
+
+    # ------- Private methods --------------
+
+    @staticmethod
+    def _get_empty_state():
+        return np.zeros((3, 3, 3), dtype=np.uint8)
--- gym-tic-tac-toe/tests/test_env.py
+++ gym-tic-tac-toe/tests/test_env.py
@@ -1,10 +1,45 @@
 from gym_tic_tac_toe.envs import TicTacToeEnv
+from gym_tic_tac_toe.envs.tic_tac_toe_env import Xt, Ot, St
+
+import unittest
+import numpy as np
+
 
 env = TicTacToeEnv()
 
 state = env.reset()
 reward = env.step(4)
 
-print(state)
-print(reward)
 
+class TestSum(unittest.TestCase):
+
+    def setUp(self):
+        self.env = TicTacToeEnv()
+
+    def test_empty_state(self):
+        empty_state = np.zeros((3, 3, 3))
+        init_state = self.env.reset()
+
+        np.testing.assert_array_equal(empty_state, init_state)
+
+    def test_state_from_string(self):
+        state_string = [
+            '_XO',
+            'O_X',
+            '___'
+        ]
+        state_arr = np.array([
+            [St, Xt, Ot],
+            [Ot, St, Xt],
+            [St, St, St]
+        ])
+
+        self.env.set_state(state_string)
+
+        internal_state = self.env.state
+
+        np.testing.assert_array_equal(state_arr, internal_state)
+
+
+if __name__ == '__main__':
+    unittest.main()
