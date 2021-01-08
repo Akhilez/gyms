4ac89b5818894037b616b51184f9b60ddb6f5913
Akhilez <lightrescuer@gmail.com>
2021-01-08 15:24:31 -0600

    testing installation of all packages

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gym-nine-mens-morris',
+      version='0.0.1',
+      install_requires=['gym']
+      )
+
+setup(name='gym-tic-tac-toe',
+      version='0.0.1',
+      install_requires=['gym']
+      )
