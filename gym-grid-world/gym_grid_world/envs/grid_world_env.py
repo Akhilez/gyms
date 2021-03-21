39a29d4d45fd75c9ec80b7354327098351256adf
Akhilez <lightrescuer@gmail.com>
2021-03-21 16:00:12 -0400

    Finished MCTS

--- gym-grid-world/gym_grid_world/envs/grid_world_env.py
+++ gym-grid-world/gym_grid_world/envs/grid_world_env.py
@@ -5,25 +5,24 @@ from gym_grid_world.envs.simple_grid_world_env.GridWorld import Gridworld
 
 
 class GridWorldEnv(gym.Env):
-
-    def __init__(self, size, mode='static'):
+    def __init__(self, size, mode="static"):
         self.size = size
         self.mode = mode
         self.done = False
         self.won = False
         self.env: Gridworld = None
-        self.actions = ['l', 'u', 'r', 'd']
+        self.actions = ["l", "u", "r", "d"]
 
     @property
     def state(self):
         return self.env.board.render_np()
 
     @state.setter
-    def state(self, positions):
-        self.env.board.components['Player'].pos = positions['player']  # Row, Column
-        self.env.board.components['Goal'].pos = positions['win']
-        self.env.board.components['Pit'].pos = positions['pit']
-        self.env.board.components['Wall'].pos = positions['wall']
+    def state(self, positions: dict):
+        self.env.board.components["Player"].pos = positions["player"]  # Row, Column
+        self.env.board.components["Goal"].pos = positions["win"]
+        self.env.board.components["Pit"].pos = positions["pit"]
+        self.env.board.components["Wall"].pos = positions["wall"]
 
     def step(self, action: int):
         self.env.makeMove(self.actions[action])
@@ -38,7 +37,7 @@ class GridWorldEnv(gym.Env):
         self.won = False
         return self.state
 
-    def render(self, mode='human'):
+    def render(self, mode="human"):
         print()
         print(self.env.board.render())
         print()
@@ -46,3 +45,29 @@ class GridWorldEnv(gym.Env):
     @staticmethod
     def get_legal_actions():
         return np.arange(4)
+
+    def get_positions(self) -> dict:
+        return self.get_positions_(self.state)
+
+    def set_state(self, state: np.ndarray):
+        positions = self.get_positions_(state)
+        self.state = positions
+
+    @staticmethod
+    def get_positions_(state: np.ndarray) -> dict:
+        player, win, pit, wall = [
+            np.array(np.nonzero(s == 1)).flatten().tolist() for s in state
+        ]
+        return {"player": player, "win": win, "pit": pit, "wall": wall}
+
+    @staticmethod
+    def is_done(state: np.ndarray) -> bool:
+        positions = GridWorldEnv.get_positions_(state)
+        player = positions["player"]
+        return player == positions["win"] or player == positions["pit"]
+
+    @staticmethod
+    def has_won(state: np.ndarray) -> bool:
+        positions = GridWorldEnv.get_positions_(state)
+        player = positions["player"]
+        return player == positions["win"]
