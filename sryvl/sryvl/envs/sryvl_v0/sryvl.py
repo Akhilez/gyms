0f86892607c717a6d521e715180276a96a01d63e
Akhilez <lightrescuer@gmail.com>
2022-09-20 11:26:11 -0400

    Finished basic env

--- sryvl/sryvl/envs/sryvl_v0/sryvl.py
+++ sryvl/sryvl/envs/sryvl_v0/sryvl.py
@@ -9,6 +9,7 @@ NOTHING = 0
 AGENT = 1
 FOOD = 2
 TERRAIN = 3
+BOUNDARY = 4
 
 ACTION_NONE = 0
 ACTION_LEFT = 1
@@ -25,73 +26,16 @@ POSITION_OFFSETS = {
 }
 
 
-class FoodBase:
-    def __init__(self, position=None, expiry_period=np.inf):
-        self.position = position
-        self.expiry_period = expiry_period
+class Food:
+    def __init__(self, position, expiry_period):
         self.age = 0
-        self.eaten = False
-
-    def expired(self):
-        return (self.age >= self.expiry_period) or self.eaten
-
-    def has_eaten(self, agent_position):
-        for agent_coord, food_coord in agent_position, self.position:
-            if agent_coord != food_coord:
-                return False
-        return True
-
-    def step(self, env, *_args, **_kwargs):
-        self.age += 1
-        if self.has_eaten(env.agent_position) or self.expired():
-            self.position = None
-
-
-class Food(FoodBase):
-    def __init__(self, position, expiry_period, growth_density, growth_radius):
-        super(Food, self).__init__(position, expiry_period)
+        self.expired = False
         self.position = position
         self.expiry_period = expiry_period
-        self.growth_density = growth_density
-        self.growth_radius = growth_radius
 
-    def step(self, env, *_args, **_kwargs):
-        more_food = []
-        growth_window = self._get_growth_window(env.world)
-        if not self._enough_food_already_exists_nearby(growth_window):
-            chance = self.age / (self.expiry_period * 2) > np.random.rand()
-            if chance:
-                more_food = [
-                    Food(
-                        self._get_random_position_nearby(growth_window),
-                        self.expiry_period,
-                        self.growth_density,
-                        self.growth_radius,
-                    )
-                ]
-        super().step(env)
-        return more_food
-
-    def _enough_food_already_exists_nearby(self, growth_window):
-        """num_food in radius < food_growth_density"""
-        num_food = len(SrYvlLvl0Env.find_indices(growth_window, FOOD)) - 1
-        return num_food < self.growth_density
-
-    @staticmethod
-    def _get_random_position_nearby(growth_window):
-        empty_positions = SrYvlLvl0Env.find_indices(growth_window, NOTHING)
-        return empty_positions[np.random.choice(len(empty_positions))]
-
-    def _get_growth_window(self, world):
-        y = self.position[0]
-        x = self.position[1]
-
-        x0 = max(0, x - self.growth_radius)
-        y0 = max(0, y - self.growth_radius)
-        x1 = min(len(world), x + self.growth_radius + 1)
-        y1 = min(len(world), y + self.growth_radius + 1)
-        
-        return world[y0:y1, x0:x1]
+    def step(self):
+        self.age += 1
+        self.expired = self.age >= self.expiry_period
 
 
 class SrYvlLvl0Env(Env):
@@ -100,7 +44,7 @@ class SrYvlLvl0Env(Env):
 
     # Set these in ALL subclasses
     action_space = Discrete(5)
-    observation_space = Box(low=0, high=3, shape=(5, 5))
+    observation_space = Box(low=0, high=4, shape=(5, 5))
 
     def __init__(
             self,
@@ -136,7 +80,8 @@ class SrYvlLvl0Env(Env):
         self.agent_position = [0, 0]
         self.foods: List[Food] = []
         self.terrain: List[Tuple[int, int]] = []
-        self.world = np.zeros((self.world_size, self.world_size))
+        self.world = np.array([])
+        self.boundary_indices: List[Tuple[int, int]] = []
 
         self.legal_actions = np.ones(self.action_space.n)
         self.done = False
@@ -144,9 +89,35 @@ class SrYvlLvl0Env(Env):
         self.reset()
 
     def render(self, mode='human'):
-        print(self.agent_size)
-        # print(self.world.astype(int))
-        print(self.observe().astype(int))
+        if mode == 'world_console':
+            print(self.agent_size)
+
+            state = '\n'.join([''.join(i) for i in self.world.astype(str)])
+            state = state.replace('4', '□')  # Boundary
+            state = state.replace('3', '■')  # Terrain
+            state = state.replace('2', '•')  # Food
+            state = state.replace('1', '🔺')  # Player
+            state = state.replace('0', ' ')  # Nothing
+            state = state.replace('.', '')
+
+            print(state)
+
+        elif mode == 'observable_console':
+            print(self.agent_size)
+
+            state = '\n'.join([' '.join(i) for i in self.observe().astype(str)])
+            state = state.replace('4', '□')  # Boundary
+            state = state.replace('3', '■')  # Terrain
+            state = state.replace('2', '•')  # Food
+            state = state.replace('1', '🔺')  # Player
+            state = state.replace('0', ' ')  # Nothing
+            state = state.replace('.', '')
+
+            print(state)
+
+        else:
+            print(self.agent_size)
+            print(self.observe())
 
     def step(self, action: int) -> Tuple[np.array, float, bool, dict]:
         """
@@ -176,17 +147,18 @@ class SrYvlLvl0Env(Env):
             self.done = True
             return self.observe(), 0, self.done, {}
 
-        shrink_rate_movement = self._get_shrink_rate_movement()
-        shrink_rate_movement *= 1 if action == ACTION_NONE else self.movement_shrink_penalty
-        self.agent_size -= shrink_rate_movement
-
         y, x = POSITION_OFFSETS[action]
         self.agent_position[0] += y
         self.agent_position[1] += x
 
-        for food in self.foods:
-            self.foods.extend(food.step(self))
+        [food.step() for food in self.foods]
         self._clear_expired_foods()
+        self._grow_agent()
+        self._grow_more_food()
+
+        shrink_rate_movement = self._get_shrink_rate_movement()
+        shrink_rate_movement *= 1 if action == ACTION_NONE else self.movement_shrink_penalty
+        self.agent_size -= shrink_rate_movement
 
         if self.agent_size <= 0:
             self.done = True
@@ -202,14 +174,17 @@ class SrYvlLvl0Env(Env):
         side = self.world_size + (self.observation_radius * 2)
         self.world = np.zeros((side, side))
 
-        food_positions = self.get_initial_food_positions(self.world_size, self.initial_food_density)
+        food_positions = self.get_initial_food_positions(side, self.initial_food_density)
         self.fill_indices(self.world, food_positions, FOOD)
 
-        self.terrain = self.make_terrain(side, self.observation_radius)
+        self.terrain = self.make_terrain(side)
         self.fill_indices(self.world, self.terrain, TERRAIN)
 
+        self.boundary_indices = self.make_boundary(side, self.observation_radius)
+        self.fill_indices(self.world, self.boundary_indices, BOUNDARY)
+
         self.foods = [
-            Food(pos, self.food_expiry_period, self.food_growth_density, self.food_growth_radius)
+            Food(pos, self.food_expiry_period)
             for pos in self.find_indices(self.world, FOOD)
         ]
 
@@ -230,6 +205,7 @@ class SrYvlLvl0Env(Env):
 
     def sample_action(self):
         mask = self.legal_actions.astype(float)
+        assert sum(mask) > 0, "No legal actions"
         return np.random.choice(len(mask), p=mask / mask.sum())
 
     def draw_env(self) -> np.array:
@@ -237,7 +213,52 @@ class SrYvlLvl0Env(Env):
         self.world = np.ones((side, side)) * NOTHING
         self.fill_indices(self.world, [f.position for f in self.foods], FOOD)
         self.fill_indices(self.world, self.terrain, TERRAIN)
+        self.fill_indices(self.world, self.boundary_indices, BOUNDARY)
         self.fill_indices(self.world, [self.agent_position], AGENT)
+
+    def _grow_more_food(self):
+        more_foods = []
+        for food in self.foods:
+            growth_window = self._get_food_growth_window(food)
+            if not self._enough_food_already_exists_nearby(growth_window):
+                chance = food.age / (self.food_expiry_period * 2) > np.random.rand()
+                if chance:
+                    more_foods.append(
+                        Food(
+                            self._get_random_position_nearby(growth_window),
+                            self.food_expiry_period,
+                        )
+                    )
+        self.foods.extend(more_foods)
+
+    def _enough_food_already_exists_nearby(self, growth_window):
+        """num_food in radius < food_growth_density"""
+        num_food = len(SrYvlLvl0Env.find_indices(growth_window, FOOD)) - 1
+        return num_food < self.food_growth_density
+
+    def _get_food_growth_window(self, food):
+        y = food.position[0]
+        x = food.position[1]
+
+        x0 = max(0, x - self.food_growth_radius)
+        y0 = max(0, y - self.food_growth_radius)
+        x1 = min(len(self.world), x + self.food_growth_radius + 1)
+        y1 = min(len(self.world), y + self.food_growth_radius + 1)
+
+        return self.world[y0:y1, x0:x1]
+
+    @staticmethod
+    def _get_random_position_nearby(growth_window):
+        empty_positions = SrYvlLvl0Env.find_indices(growth_window, NOTHING)
+        return empty_positions[np.random.choice(len(empty_positions))]
+
+    def _grow_agent(self):
+        new_size = self.agent_size + self.agent_growth_rate
+        if new_size <= self.max_agent_size:
+            for i, food in enumerate(self.foods):
+                if food.position[0] == self.agent_position[0] and food.position[1] == self.agent_position[1]:
+                    self.agent_size = new_size
+                    del self.foods[i]
 
     def _clear_expired_foods(self):
         self.foods = [food for food in self.foods if food.position is not None]
@@ -246,8 +267,9 @@ class SrYvlLvl0Env(Env):
     def _find_legal_actions(self):
         """
         - If agent's health is 0, all actions are illegal.
-        - If agent has a wall in a direction d, then d is illegal.
-        (All boundaries are walls)
+        - If agent has a BOUNDARY in a direction d, then d is illegal.
+        - If agent has a terrain in a direction d:
+            - if the agent size < threshold, then d is illegal
         """
         if self.agent_size <= 0:
             return np.zeros(self.action_space.n)
@@ -257,14 +279,25 @@ class SrYvlLvl0Env(Env):
         y = self.agent_position[0]
         x = self.agent_position[1]
 
-        if self.world[y, x - 1] == TERRAIN:
+        if self.world[y, x - 1] == BOUNDARY:
             left = 0
-        if self.world[y - 1, x] == TERRAIN:
+        if self.world[y - 1, x] == BOUNDARY:
             up = 0
-        if self.world[y, x + 1] == TERRAIN:
+        if self.world[y, x + 1] == BOUNDARY:
             right = 0
-        if self.world[y + 1, x] == TERRAIN:
+        if self.world[y + 1, x] == BOUNDARY:
             down = 0
+
+        if self.agent_size < self.size_threshold_to_jump:
+            if self.world[y, x - 1] == TERRAIN:
+                left = 0
+            if self.world[y - 1, x] == TERRAIN:
+                up = 0
+            if self.world[y, x + 1] == TERRAIN:
+                right = 0
+            if self.world[y + 1, x] == TERRAIN:
+                down = 0
+
         return np.array([nothing, left, up, right, down])
 
     def _get_shrink_rate_movement(self):
@@ -279,22 +312,24 @@ class SrYvlLvl0Env(Env):
         return np.array((world == category).nonzero()).T
 
     @staticmethod
-    def make_terrain(world_size, observation_radius) -> List[Tuple[int, int]]:
+    def make_terrain(world_size) -> List[Tuple[int, int]]:
         """
-        All boundaries are walls.
         Add perlin noise for terrain, then another layer of perlin noise as nothing.
         """
         # TODO Get perlin noise
         terrain = np.random.random((world_size, world_size))
         terrain = (terrain < 0.1) * TERRAIN
-
-        # Add terrain padding
-        terrain[:observation_radius, :] = TERRAIN
-        terrain[-observation_radius:, :] = TERRAIN
-        terrain[:, :observation_radius] = TERRAIN
-        terrain[:, -observation_radius:] = TERRAIN
-
         return np.array((terrain == TERRAIN).nonzero()).T
+
+    @staticmethod
+    def make_boundary(world_size, observation_radius) -> List[Tuple[int, int]]:
+        # Add boundary padding
+        boundary = np.zeros((world_size, world_size))
+        boundary[:observation_radius, :] = BOUNDARY
+        boundary[-observation_radius:, :] = BOUNDARY
+        boundary[:, :observation_radius] = BOUNDARY
+        boundary[:, -observation_radius:] = BOUNDARY
+        return np.array((boundary == BOUNDARY).nonzero()).T
 
     @staticmethod
     def get_initial_food_positions(world_size, initial_food_density) -> List[Tuple[int, int]]:
@@ -310,13 +345,22 @@ class SrYvlLvl0Env(Env):
 
 
 def main():
-    env = SrYvlLvl0Env(initial_food_density=0.2)
-    env.reset()
-    env.render()
+    env = SrYvlLvl0Env(initial_food_density=0.2, world_size=10)
+    env.render(mode='world_console')
     for i in range(10):
-        env.render()
         env.step(env.sample_action())
+        env.render(mode='world_console')
+
+
+def human_play():
+    env = SrYvlLvl0Env(initial_food_density=0.2, world_size=10)
+    while not env.done:
+        env.render(mode='world_console')
+        inp = input('wasde input: ')
+        key = 'eawds'
+        action = key.index(inp)
+        env.step(action)
 
 
 if __name__ == '__main__':
-    main()
+    human_play()
