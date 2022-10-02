33c61aaaa59bfa68b9698eac90bbe49b7c6fb0de
Akhilez <lightrescuer@gmail.com>
2022-10-01 19:40:27 -0700

    tiny bug fix

--- sryvl/sryvl/__init__.py
+++ sryvl/sryvl/__init__.py
@@ -3,5 +3,5 @@ from gym.envs.registration import register
 
 register(
     id='sryvl-v0',
-    entry_point='sryvl.envs.sryvl_v0.sryvl.SrYvlEnv',
+    entry_point='sryvl.envs.sryvl_v0.sryvl:SrYvlLvl0Env',
 )
