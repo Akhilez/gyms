f16120bf0aaefa860e2f74e17880de6827fb72cc
Akhilez <lightrescuer@gmail.com>
2023-02-14 11:01:41 -0500

    Planning to start maddpg.

--- gym-nine-mens-morris/setup.py
+++ gym-nine-mens-morris/setup.py
@@ -4,5 +4,5 @@ setup(
       name='gym_nine_mens_morris',
       version='0.0.1',
       install_requires=['gymnasium'],
-      packages=['gym_nine_mens_morris/', 'nmm/'],
+      packages=['gym_nine_mens_morris/', 'nine_mens_morris/'],
 )
